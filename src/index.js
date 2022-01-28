37fd932485ea0124e9abca686c993a407d447952
nacho <nacholeon30@hotmail.com>
2022-01-28 12:51:55 -0300

    finish background styling, add loading feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './reset.css';
 import './style.css';
+import icon1 from './black-24dp/2x/outline_refresh_black_24dp.png';
 
+const loadingIcon = document.querySelector('#loading');
+loadingIcon.src = icon1;
 const fetchWeather = async function(city) {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=433cc5baf54233a15ed6c8c968fd822c`, {mode: 'cors'});
   const fetchedData = await response.json();
@@ -37,11 +40,14 @@ const displayWeather = function(weather) {
 
 const input = document.querySelector('input');
 const getWeather = function() {
+  loadingIcon.classList.remove('hidden');
   fetchWeather(input.value)
     .then(function(data) {
+      loadingIcon.classList.add('hidden');
       displayWeather(parseWeather(data));
     })
     .catch(function() {
+      loadingIcon.classList.add('hidden');
       alert('Please, enter a valid city.');  // I could do better but I don't want to spend much time on this for now.
     });
 }
